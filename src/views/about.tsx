4814065dcfa1e04f68893613d5d9dd61cf882a6f
swa-ne <spaulbautista@gmail.com>
2024-01-31 02:26:17 +0800

    Finalize the header design, but no animation

--- src/views/about.tsx
+++ src/views/about.tsx
@@ -1,3 +1,4 @@
+import '../assets/scss/about.scss';
 const About: React.FC = () => {
     return (
         <div className='about-container' id='about'>
--- src/views/header.tsx
+++ src/views/header.tsx
@@ -5,13 +5,13 @@ const Header: React.FC = () => {
     return (
         <header className='header-holder flex flex-col-reverse justify-around px-8 py-4 ml:py-4 md:flex-row md:justify-around md:items-center' id='home'>
             <div className='w-full h-1/2 md:h-3/5 md:w-6/12'>
-                <span className='text-xl ml:text-3xl/10 md:text-4xl font-medium'>
+                <span className='text-xl ml:text-3xl/10 md:text-4xl xl:text-5xl font-medium'>
                     <label className='text-accent-color'>Hello,</label> I'm
                 </span>
-                <h1 className='w-full text-2xl/10 font-black ml:text-5xl/10 md:text-6xl lg:text-7xl title-name-header'>
+                <h1 className='w-full text-2xl/10 font-black ml:text-5xl/10 md:text-6xl lg:text-7xl xl:text-8xl title-name-header'>
                     <ReactTyped strings={['Web Developer', 'Stephen Bautista']} typeSpeed={100} backSpeed={30} />
                 </h1>
-                <p className='text-xs lg:text-sm text-justify w-11/12'>An ambitious problem solver with a passion for web development, and who would like to collaborate with you and on diverse web development ideas.</p>
+                <p className='text-xs lg:text-sm xl:text-base text-justify w-11/12'>An ambitious problem solver with a passion for web development, and who would like to collaborate with you and on diverse web development ideas.</p>
                 <div className='w-2/3 h-1/2 header-button-holder mx-auto flex flex-col justify-around md:h-auto md:flex-row md:w-full xl:w-1/2'>
                     {/* TODO: able to download cv and direct hire me to contact page */}
                     <button className='download-button'>Download CV</button>

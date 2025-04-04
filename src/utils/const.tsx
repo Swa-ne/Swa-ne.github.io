b3189bed0c0c258b79b3d8dfef61dbd8b7b04977
Swa-ne <spaulbautista@gmail.com>
2025-04-04 19:07:09 +0800

    Updated my projects

--- src/utils/const.tsx
+++ src/utils/const.tsx
@@ -1,8 +1,11 @@
+import Sakay from '../assets/image/project/sakay.png';
 import Unilodge from '../assets/image/project/Unilodge.jpg';
 import FundChain from '../assets/image/project/FundChain.jpg';
 import MessagingApp from '../assets/image/project/messaging-app.jpg';
 import PhinmaHub from '../assets/image/project/phinma-hub.jpg';
-import NetflixClone from '../assets/image/project/netflix-clone.jpg';
+import PhinmaPulse from '../assets/image/project/phinma-pulse.png';
+import NetflixClone from '../assets/image/project/netflix-clone.png';
+import WheelWander from '../assets/image/project/wheel-wander.png';
 import Wordle from '../assets/image/project/wordle.jpg';
 
 interface projectDataInterface {
@@ -16,50 +19,74 @@ interface projectDataInterface {
 export const projectData: Array<projectDataInterface> = [
     {
         id: 1,
-        name: 'Unilodge',
-        img: Unilodge,
-        tech: 'Flutter | NodeJS | MongoDB',
-        repo: 'https://github.com/Swa-ne/UniLodge',
-        link: 'https://github.com/Swa-ne/UniLodge',
-    },
-    {
-        id: 2,
-        name: 'FundChain',
-        img: FundChain,
-        tech: 'ReactJS | TypeScript | NodeJS | WalletConnect',
-        repo: 'https://github.com/ITHINKERBELL/ITHINKERBELL---FundChain',
-        link: 'https://github.com/ITHINKERBELL/ITHINKERBELL---FundChain',
-    },
-    {
-        id: 3,
-        name: 'Messaging App',
-        img: MessagingApp,
-        tech: 'Python | Kivy | SQLite',
-        repo: 'https://github.com/Swa-ne/messagingApp',
-        link: 'https://messagingappclient.onrender.com',
-    },
-    {
-        id: 4,
-        name: 'PHINMA Hub',
-        img: PhinmaHub,
-        tech: 'ReactJS | React Native | NodeJS | MongoDB',
-        repo: 'https://github.com/Zaisuki/PHubFinalProj',
-        link: 'https://github.com/Zaisuki/PHubFinalProj',
-    },
-    {
-        id: 5,
-        name: 'Netflix Clone',
-        img: NetflixClone,
-        tech: 'React JS | CSS',
-        repo: 'https://github.com/Swa-ne/netflix-clone',
-        link: 'https://github.com/Swa-ne/netflix-clone',
-    },
-    {
-        id: 6,
         name: 'Ultimate Wordle',
         img: Wordle,
         tech: 'Python | Kivy | SQLite',
         repo: 'https://github.com/Swa-ne/fWordle',
         link: 'https://github.com/Swa-ne/fWordle',
+    },
+    {
+        id: 2,
+        name: 'Netflix Clone',
+        img: NetflixClone,
+        tech: 'JavaScript | React.js | Sass',
+        repo: 'https://github.com/Swa-ne/netflix-clone',
+        link: 'https://github.com/Swa-ne/netflix-clone',
+    },
+    {
+        id: 3,
+        name: 'Wheel Wander',
+        img: WheelWander,
+        tech: 'Figma | TypeScript | Kotlin | Android Studio | Node.js | Express.js | MySQL',
+        repo: 'https://github.com/Swa-ne/Wheel-Wander',
+        link: 'https://github.com/Swa-ne/Wheel-Wander',
+    },
+    {
+        id: 4,
+        name: 'PHINMA Pulse',
+        img: PhinmaPulse,
+        tech: 'Figma | TypeScript | React.js | React Native | Node.js | Express.js | MongoDB',
+        repo: 'https://github.com/Swa-ne/HATAKONTITANS',
+        link: 'https://github.com/Swa-ne/HATAKONTITANS',
+    },
+    {
+        id: 5,
+        name: 'FundChain',
+        img: FundChain,
+        tech: 'Figma | TypeScript | React.js | Node.js | Express.js | MongoDB | ReOwn',
+        repo: 'https://github.com/ITHINKERBELL/ITHINKERBELL---FundChain',
+        link: 'https://github.com/ITHINKERBELL/ITHINKERBELL---FundChain',
+    },
+    {
+        id: 6,
+        name: 'Messaging App',
+        img: MessagingApp,
+        tech: 'TypeScript | React.js | Node.js | Express.js | MongoDB',
+        repo: 'https://github.com/Swa-ne/messagingApp',
+        link: 'https://messagingappclient.onrender.com',
+    },
+    {
+        id: 7,
+        name: 'PHINMA Hub',
+        img: PhinmaHub,
+        tech: 'Figma | React.js | React Native | TypeScript | Node.js | Express.js | MongoDB',
+        repo: 'https://github.com/Zaisuki/PHubFinalProj',
+        link: 'https://github.com/Zaisuki/PHubFinalProj',
+    },
+    {
+        id: 8,
+        name: 'Unilodge',
+        img: Unilodge,
+        tech: 'Figma | Flutter | TypeScript | Node.js | Express.js | MongoDB | Python',
+        repo: 'https://github.com/Swa-ne/UniLodge',
+        link: 'https://github.com/Swa-ne/UniLodge',
+    },
+    {
+        id: 9,
+        name: 'Sakay',
+        img: Sakay,
+        tech: 'Figma | Flutter | TypeScript | Node.js | Express.js | MongoDB | Python | Redis',
+        repo: 'https://github.com/Swa-ne/Sakay',
+        link: 'https://github.com/Swa-ne/Sakay',
     },
 ];
--- src/views/project.tsx
+++ src/views/project.tsx
@@ -7,9 +7,12 @@ const Project: React.FC = () => {
         <div className='project-container' id='project'>
             <h1 className='text-text-color text-center text-6xl mb-6'>Project</h1>
             <div className='projects-holder'>
-                {projectData.map((data, idx) => (
-                    <ProjectList key={idx} projectTitle={data.name} projectImg={data.img} projectTech={data.tech} projectLink={data.link} projectRepo={data.repo} />
-                ))}
+                {projectData
+                    .slice()
+                    .reverse()
+                    .map((data, idx) => (
+                        <ProjectList key={idx} projectTitle={data.name} projectImg={data.img} projectTech={data.tech} projectLink={data.link} projectRepo={data.repo} />
+                    ))}
             </div>
         </div>
     );
